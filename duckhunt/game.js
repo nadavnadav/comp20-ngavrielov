986f5e400fb654800a3dcbb54d13b5dd4cf7b0df
Nadav93 <nadav93@gmail.com>
2014-02-24 19:40:32 -0500

    updated the dirt road

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,13 @@
 // Your work goes here...
+
+function draw(){
+var canvas = document.getElementById('game');
+var ctx = canvas.getContext('2d');
+
+
+
+		ctx.fillStyle = "#C96A1B";
+        ctx.fillRect (0, 548, 900, 52);
+
+
+}
