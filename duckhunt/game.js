fb54cb0691b61724f31b3ab4d75b7fe0d9ed632f
Nadav93 <nadav93@gmail.com>
2014-02-24 20:41:14 -0500

    added all of the birds

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -14,7 +14,12 @@ var ctx = canvas.getContext('2d');
     imageObj.onload = function() {
     ctx.drawImage(imageObj, 0, 700, 900, 150, 0, 412, 900, 150); //bushes
     ctx.drawImage(imageObj, 0, 0, 60, 43, 100, 530, 60, 43); //dog
-    ctx.drawImage(imageObj, 0, 0, 60, 43, 100, 530, 60, 43); //tree
+    ctx.drawImage(imageObj, 0, 273, 84, 124, 300, 450, 84, 124); //tree
+	ctx.drawImage(imageObj, 0, 119, 39, 32, 280, 20, 39, 32); //bird_1
+	ctx.drawImage(imageObj, 134, 196, 31, 31, 400, 100, 31, 31); //bird_2
+	ctx.drawImage(imageObj, 337, 155, 38, 38, 30, 300, 38, 38); //bird_3
+	ctx.drawImage(imageObj, 166, 118, 40, 30, 20, 40, 40, 30); //bird_4
+	ctx.drawImage(imageObj, 81, 193, 36, 36, 600, 70, 36, 36); //bird_5
     //ctx.drawImage(imageObj, xcoord_orig, ycoord_orig, width, height, desti_x_coord, desti_y_coord, destination_width, destination_height)
     };
 
