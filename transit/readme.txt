f82363eac0e53103a09f27709b0beb0fae8307c9
Nadav93 <nadav93@gmail.com>
2014-03-13 15:02:31 -0400

    updated to includ readme

---
+++ transit/readme.txt
@@ -0,0 +1,7 @@
+Nadav Gavrielov's MBTA Map:
+
+Loads a map based off of the user's location, which is denoted by an elephant.
+Depending on the results of the MBTAMap JSON, it renders the correct MBTA line on the map.
+The stations are denoted by the color icons. The stations are connected by an appropriately colored line.
+The station name can be found by clicking on the station.
+Not functional yet (but will be after break): train schedules and the 'closest station' feature.
--- transit/transit.js
+++ transit/transit.js
@@ -11,6 +11,7 @@
 			var infowindow = new google.maps.InfoWindow();
 			var places;
 			var request;
+			stopInfo = [];
 			
 			function init()
 			{
@@ -32,16 +33,16 @@
 	// 4 = complete
 		if (request.readyState == 4 && request.status == 200) {
 			scheduleData = JSON.parse(request.responseText);
-			console.log(scheduleData.line);
+			//console.log(scheduleData.line);
 			color = scheduleData.line;
-			lineDrawer(color);
+			lineDrawer(color, scheduleData);
 		}
 		else if (request.readyState == 4 && request.status == 500){
 			//failed to load
 		}
 }
 
-			function lineDrawer(color){     
+			function lineDrawer(color, scheduleData){     
 					var orangelines = '[{"color":"orange","name":"Oak Grove","lat":42.43668,"lng":-71.071097},{"color":"orange","name":"Malden Center","lat":42.426632,"lng":-71.07411},{"color":"orange","name":"Wellington","lat":42.40237,"lng":-71.077082},{"color":"orange","name":"Sullivan","lat":42.383975,"lng":-71.076994},{"color":"orange","name":"Community College","lat":42.373622,"lng":-71.069533},{"color":"orange","name":"North Station","lat":42.365577,"lng":-71.06129},{"color":"orange","name":"Haymarket","lat":42.363021,"lng":-71.05829},{"color":"orange","name":"State Street","lat":42.358978,"lng":-71.057598},{"color":"orange","name":"Downtown Crossing","lat":42.355518,"lng":-71.060225},{"color":"orange","name":"Chinatown","lat":42.352547,"lng":-71.062752},{"color":"orange","name":"Tufts Medical","lat":42.349662,"lng":-71.063917},{"color":"orange","name":"Back Bay","lat":42.34735,"lng":-71.075727},{"color":"orange","name":"Mass Ave","lat":42.341512,"lng":-71.083423},{"color":"orange","name":"Ruggles","lat":42.336377,"lng":-71.088961},{"color":"orange","name":"Roxbury Crossing","lat":42.331397,"lng":-71.095451},{"color":"orange","name":"Jackson Square","lat":42.323132,"lng":-71.099592},{"color":"orange","name":"Stony Brook","lat":42.317062,"lng":-71.104248},{"color":"orange","name":"Green Street","lat":42.310525,"lng":-71.107414},{"color":"orange","name":"Forest Hills","lat":42.300523,"lng":-71.113686}]';
 					var blueLines = '[{"color":"blue","name":"Wonderland","lat":42.41342,"lng":-70.991648},{"color":"blue","name":"Revere Beach","lat":42.40784254,"lng":-70.99253321},{"color":"blue","name":"Beachmont","lat":42.39754234,"lng":-70.99231944},{"color":"blue","name":"Suffolk Downs","lat":42.39050067,"lng":-70.99712259},{"color":"blue","name":"Orient Heights","lat":42.386867,"lng":-71.004736},{"color":"blue","name":"Wood Island","lat":42.3796403,"lng":-71.02286539},{"color":"blue","name":"Airport","lat": 42.374262,"lng":-71.030395},{"color":"blue","name":"Maverick","lat":42.36911856,"lng":-71.03952958},{"color":"blue","name":"Aquarium","lat":42.359784,"lng":-71.051652},{"color":"blue","name":"State Street","lat":42.358978,"lng":-71.057598},{"color":"blue","name":"Government Center","lat":42.359705,"lng":-71.059215},{"color":"blue","name":"Bowdoin","lat":42.361365,"lng":-71.062037}]';
 					var redLines = '[{"color":"red","name":"Alewife","lat":42.395428,"lng":-71.142483},{"color":"red","name":"Davis","lat":42.39674,"lng":-71.121815},{"color":"red","name":"Porter Square","lat":42.3884,"lng":-71.119149},{"color":"red","name":"Harvard Square","lat":42.373362,"lng":-71.118956},{"color":"red","name":"Central Square","lat":42.365486,"lng":-71.103802},{"color":"red","name":"Kendall/MIT","lat":42.36249079,"lng":-71.08617653},{"color":"red","name":"Charles/MGH","lat":42.361166,"lng":-71.070628},{"color":"red","name":"Park Street","lat":42.35639457,"lng":-71.0624242},{"color":"red","name":"Downtown Crossing","lat":42.355518,"lng":-71.060225},{"color":"red","name":"South Station","lat":42.352271,"lng":-71.055242},{"color":"red","name":"Broadway","lat":42.342622,"lng":-71.056967},{"color":"red","name":"Andrew","lat":42.330154,"lng":-71.057655},{"color":"red","name":"JFK/UMass","lat":42.320685,"lng":-71.052391},{"color":"red","name":"Savin Hill","lat":42.31129,"lng":-71.053331},{"color":"red","name":"Fields Corner","lat":42.300093,"lng":-71.061667},{"color":"red","name":"Shawmut","lat":42.29312583,"lng":-71.06573796},{"color":"red","name":"Ashmont","lat":42.284652,"lng":-71.064489},{"color":"red","name":"North Quincy","lat":42.275275,"lng":-71.029583},{"color":"red","name":"Wollaston","lat":42.2665139,"lng":-71.0203369},{"color":"red","name":"Quincy Center","lat":42.251809,"lng":-71.005409},{"color":"red","name":"Quincy Adams","lat":42.233391,"lng":-71.007153},{"color":"red","name":"Braintree","lat":42.2078543,"lng":-71.0011385}]';
@@ -55,29 +56,19 @@
 					orangeparsed = JSON.parse(orangelines);
 					blueparsed = JSON.parse(blueLines);
 					redparsed = JSON.parse(redLines);
-					orange_icon = 'orange_icon1.png';
+					orange_icon = 'orange_icon1.png'; //I made these
 					blue_icon = 'blue_icon.png';
 					red_icon = 'red_icon1.png';
 					if (color == 'orange'){     
 						for (i=0; i < 19; i++)
 						{
-							console.log(i);
 							marker_a = new google.maps.LatLng(orangeparsed[i].lat, orangeparsed[i].lng);
 							orange_positions.push(marker_a);
 							var station_name = orangeparsed[i].name;
+							stops(scheduleData, station_name);
 							marker_set(marker_a, station_name, orange_icon);
-							/*orange_markers.push(new google.maps.Marker({
-								position: marker_a,
-								title: "hi",
-								icon: orange_icon,
-							}));*/
 						} 
 						for (i = 0; i<orange_markers.length;i++) {
-							/*google.maps.event.addListener(orange_markers[i], 'click', function() {
-							infowindow.setContent("hi");
-							infowindow.open(map, orange_markers[i]);
-							});*/
-							console.log(i);
 							orange_markers[i].setMap(map);
 						}
 
@@ -89,15 +80,6 @@
 						    strokeWeight: 2
 						  });
 						orangePolyLine.setMap(map);
-
-						/*for (i = 0; i < orangeparsed.length; i++)
-						{
-							var test123 = orangeparsed[i].name;
-							google.maps.event.addListener(orange_markers[i], 'click', function() {
-							infowindow.setContent(test123);
-							infowindow.open(map, this);
-							});
-						}*/
 					}
 
 					function marker_set(marker, name, icon)
@@ -119,7 +101,6 @@
 					if (color == 'blue'){
 						for (i=0; i < 12; i++)
 						{
-							console.log(i);
 							marker_b = new google.maps.LatLng(blueparsed[i].lat, blueparsed[i].lng);
 							blue_positions.push(marker_b);
 							var station_name = blueparsed[i].name;
@@ -132,7 +113,6 @@
 
 						} 
 						for (i = 0; i<blue_markers.length;i++) {
-							console.log(i);
 							blue_markers[i].setMap(map);
 						}
 						bluePolyLine= new google.maps.Polyline({
@@ -148,7 +128,6 @@
 					if (color == 'red'){     
 						for (i=0; i < 22; i++)
 						{
-							console.log(i);
 							marker_c = new google.maps.LatLng(redparsed[i].lat, redparsed[i].lng);
 							var station_name = redparsed[i].name;
 							marker_set(marker_c, station_name, red_icon);
@@ -177,7 +156,6 @@
 						}
 						for (i = 0; i<red_a_markers.length;i++) {
 
-							console.log(i);
 							red_a_markers[i].setMap(map);
 						}
 						redPolyLineA= new google.maps.Polyline({
@@ -201,6 +179,23 @@
 					}
 			}
 
+			
+			function stops(scheduleData, station_name){
+				/*for (i = 0; i < scheduleData.schedule.length; i++)
+				{
+					trip = scheduleData.schedule[i];
+					//console.log(trip);
+					s = trip["Predictions"];
+					for (j = 0; j < s.length; j++){
+						//stationThis = s[j];
+						if (stationThis["Stop"] == station_name){
+							//The info box for station["stop"] gets the direction and the time remaining
+						}
+					}
+				}*/
+			}
+
+
 			function getMyLocation()
 			{
 				if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
@@ -220,7 +215,8 @@
 				me = new google.maps.LatLng(userLat, userLng);
 				// Update map and go there...
 				map.panTo(me);
-				var icon_image = 'https://cdn4.iconfinder.com/data/icons/48x48-free-object-icons/48/Elephant.png';
+				//Elephant image comes from https://cdn4.iconfinder.com/data/icons/48x48-free-object-icons/48/Elephant.png
+				var icon_image = 'elephant.png';
 				// Create a marker
 				marker = new google.maps.Marker({
 					position: me,
